9667ed557c6a4a6ff0103c870850f85d2ed1cf09
Stephen Appiah <appstephen8@gmail.com>
2024-03-31 15:04:08 +0000

    content edits

--- components/Header.vue
+++ components/Header.vue
@@ -18,7 +18,7 @@ const links = [{
 <template>
   <UHeader :links="links">
     <template #logo>
-      Nuxt UI Pro <UBadge label="SaaS" variant="subtle" class="mb-0.5" />
+      Homestud <UBadge label="real estate management software" variant="subtle" class="mb-0.5" />
     </template>
 
     <template #right>
